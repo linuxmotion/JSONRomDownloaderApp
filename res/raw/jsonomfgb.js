b9b7e57dcbf8391833a3be3ce6647533247c37d9
LinuxMotion <jweyrauch359@gmail.com>
2011-07-07 22:52:26 -0700

    Initial commit of the resources for the project

---
+++ res/raw/jsonomfgb.js
@@ -0,0 +1,21 @@
+[
+{
+"device"="Incredible",
+"base"="1.3.0",
+"version"=3,
+"name"="OMFGB-inc-1.3.0-3.zip",
+"date"="Some date",
+"url"="http://bit.ly/l4tntd",
+"installable"="false"
+},
+
+{
+"device"="Incredible",
+"base"="1.3.0",
+"version"=2,
+"name"="OMFGBk-sbc-1.apk",
+"date"="Some date",
+"url"="http://r2doesinc.bitsurge.net/Addons/OMFGBk-sbc-1.zip",
+"installable"="true"
+}
+]
