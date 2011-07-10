c5cf0b9cce1cfcfeb76162a360e05fb3df63c675
LinuxMotion <jweyrauch359@gmail.com>
2011-07-10 15:42:02 -0700

    Refactor JSONPrefernceScreenView into OMFGBExternalAddonsApp Add a Constants class and update references with the new class

---
+++ src/com/t3hh4xx0r/external/addons/utils/Constants.java
@@ -0,0 +1,47 @@
+package com.t3hh4xx0r.external.addons.utils;
+
+import java.io.File;
+
+import android.os.Environment;
+
+public final class Constants {
+	
+	
+	/**
+	 *  Variable to turn debuging on across the app
+	 */
+	public static boolean FULL_DBG = false;
+	/**
+	 *  External storage mount point
+	 *  @hide
+	 * 
+	 */
+	private static File extStorageDirectory = Environment.getExternalStorageDirectory();
+	
+	/**
+	 * Basic OMFGB downloads directory
+	 */
+    public static final String DOWNLOAD_DIR = extStorageDirectory + "/t3hh4xx0r/downloads";
+    
+    /**
+     * ClockWorkMod Recovery flash directory 
+     */
+    public static final String CWR_FLASH_DIR = "/sdcard/t3hh4xx0r/downloads/";
+    
+    /**
+     * System app location
+     */
+    public static final String SYSTEM_APP = "/system/app/";
+    
+    /**
+     * ClockWorkMod Recovery extended command directory
+     */
+    public static final String CWR_EXTENDED_CMD = "/cache/recovery/extendedcommand";
+    
+    /**
+     *  Base URL for the manifest files for the addons, nightlies, and eventually the release roms.
+     */
+    public static final String BASE_SCRIPT_URL = "https://raw.github.com/OMFGB/OMFGBManifests/master/"; 
+	
+
+}
